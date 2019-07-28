b27153b5db141bfc17f2928c57d71b8e83b91257
Colin Lu <clu@apptium.com>
2019-07-28 14:39:50 -0400

    update the README.md

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 # A Configuration-based Dependency Injection Framework for Building Deep Learning Model on TFLearn
+
 This is a sample project of "Smart Customer Retention" building deep learning models using a configuration-based dependency injection framework on TFLearn.
+
+---
+
+## Overview
+This sample consists of the following parts:
+* __fraework__
+
+	A dependency injection frameowrk with pre-built python modules to build deep learning models on TFLearn and TensorFlow.
+	
+* __app__
+
+    A sample application of "Smart Customer Retntion" having two classifiers of predicting customer churn and providing customer retention recommendations.
+    
+---
+
+## How to Use
+### Prerequisite
+
+* [Python](https://www.python.org/)
+* [TensorFlow](https://www.tensorflow.org/)
+* [TFLearn](http://tflearn.org/)
+* [PyYAML](https://pyyaml.org/wiki/PyYAMLDocumentation)
+
+### Build and Traing Deep Learning Model using the Framework
+#### Customer Churn
+##### Dataset
+The sample dataset can be found as di-framework-tflearn/app/data/customer_churn.csv
+![](http://)
+
 
 
--- app/application.py
+++ app/application.py
@@ -1,3 +1,4 @@
+import sys
 from framework.application_context import ApplicationContext
 
 
@@ -11,3 +12,33 @@ class CustomerRetentionApp:
     def predict(self, name, data):
         return self.application.get_classifier(name).predict(data)
 
+
+if __name__ == '__main__':
+    app = CustomerRetentionApp()
+    if sys.argv[1] == 'build_model':
+        app.build_model(sys.argv[2])
+    elif sys.argv[1] == 'predict':
+        tony = ['4d316bef-9856-4ea0-aed0-a53e55fed3db', 'Male', 'K-12 student', 20, 'No', 'Yes', 12, 'No', 'No',
+                    'Wireless']
+        john = ['4d316bef-9856-4ea0-aed0-a53e55fed3df', 'Male', 'Sales and Related', 75, 'Yes', 'Yes', 26, 'Yes',
+                    'No', 'Wireless']
+        mary = ['7150ae6c-1120-4eb5-b788-0f822f986fae', 'Female', 'Executive/Management', 90, 'No', 'No', 36, 'Yes',
+                    'Yes', 'Bundle']
+        result = app.predict(sys.argv[2], [tony, john, mary])
+        if sys.argv[2] == 'customer_churn':
+            print("Customer churn prediction for Tony - churn:{} - score:{}".format(result[0].churn, result[0].score))
+            print("Customer churn prediction for John - churn:{} - score:{}".format(result[1].churn, result[1].score))
+            print("Customer churn prediction for Mary - churn:{} - score:{}".format(result[2].churn, result[2].score))
+        else:
+            print("Top3 retention recommendations for Tony: \n 1: {} \n 2: {}  \n 3: {}".format(
+                result[0].recommendations[0],
+                result[0].recommendations[1],
+                result[0].recommendations[2]))
+            print("Top3 retention recommendations for John: \n 1: {} \n 2: {}  \n 3: {}".format(
+                result[1].recommendations[0],
+                result[1].recommendations[1],
+                result[1].recommendations[2]))
+            print("Top3 retention recommendations for Mary: \n 1: {} \n 2: {}  \n 3: {}".format(
+                result[2].recommendations[0],
+                result[2].recommendations[1],
+                result[2].recommendations[2]))
